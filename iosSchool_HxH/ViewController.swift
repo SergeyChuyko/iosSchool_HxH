76afee89e3688b4253167b69ce37958e747bca53
SergeyChuyko <frodo.logon.com@mail.ru>
2023-10-19 15:10:47 +0700

    Create Project 1

---
+++ iosSchool_HxH/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iosSchool_HxH
+//
+//  Created by Sergo on 19.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
