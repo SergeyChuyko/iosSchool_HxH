19a763eeb8651631e16389a9e96a966b43acaa61
SergeyChuyko <frodo.logon.com@mail.ru>
2023-10-30 16:47:20 +0700

    Homework1

--- iosSchool_HxH/Homeworks/firstHomework.swift
+++ iosSchool_HxH/Homeworks/firstHomework.swift
@@ -6,23 +6,26 @@
 //
 
 import Foundation
+
 struct Location {
-    var id: Int
-    var name: String
-    var type: String
-    var dimension: String
-    var residents = [String]()
+    let id: Int
+    let name: String
+    let type: String
+    let dimension: String
+    let residents: [String]
 }
+
 struct LocationsList {
+    struct Info {
+        let count: Int
+        let pages: Int
+        let next: String?
+        let prev: String?
+    }
     let info: Info
     let results: [Location]
 }
-struct Info {
-    let count: Int
-    let pages: Int
-    let next: String?
-    let prev: String?
-}
+
 class Character {
     let id: Int
     let name: String
@@ -32,14 +35,27 @@ class Character {
     let episode: [String]
     let gender: Gender
     let status: Status
-    init(id: Int,
-         name: String,
-         species: String,
-         image: String,
-         url: String,
-         episode: [String],
-         gender: Gender,
-         status: Status) {
+    enum Gender: String, CaseIterable {
+        case female = "Female"
+        case male = "Male"
+        case genderless = "Genderless"
+        case unknown = "unknown"
+    }
+    enum Status: String, CaseIterable {
+        case alive = "Alive"
+        case dead = "Dead"
+        case unknown = "unknown"
+    }
+    init(
+        id: Int,
+        name: String,
+        species: String,
+        image: String,
+        url: String,
+        episode: [String],
+        gender: Gender,
+        status: Status
+    ) {
         self.id = id
         self.name = name
         self.species = species
@@ -50,17 +66,7 @@ class Character {
         self.status = status
     }
 }
-enum Gender: String, CaseIterable {
-    case female = "Female"
-    case male = "Male"
-    case genderless = "Genderless"
-    case unknown = "Unknown"
-}
-enum Status: String, CaseIterable {
-    case alive = "Alive"
-    case dead = "Dead"
-    case unknown = "Unknown"
-}
+
 class CharacterGenerator {
     func generateSomeCharacter() -> Character {
         let idCharacter = Int.random(in: 1...99)
@@ -69,15 +75,17 @@ class CharacterGenerator {
         let image = "\(idCharacter).JPEG"
         let url = "CharacterURL: https://www.\(idCharacter).com"
         let episode = ["Episode number: \(idCharacter)"]
-        let gender = Gender.allCases.randomElement() ?? .unknown
-        let status = Status.allCases.randomElement() ?? .unknown
-        return Character(id: idCharacter,
-                         name: name,
-                         species: species,
-                         image: image,
-                         url: url,
-                         episode: episode,
-                         gender: gender,
-                         status: status)
+        let gender = Character.Gender.allCases.randomElement() ?? .unknown
+        let status = Character.Status.allCases.randomElement() ?? .unknown
+        return Character(
+            id: idCharacter,
+            name: name,
+            species: species,
+            image: image,
+            url: url,
+            episode: episode,
+            gender: gender,
+            status: status
+        )
     }
 }
