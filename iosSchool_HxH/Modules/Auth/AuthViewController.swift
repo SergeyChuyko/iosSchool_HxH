9656cc4446b3ec7bfe754b6d3c91bfea52d76e4d
SergeyChuyko <frodo.logon.com@mail.ru>
2023-11-09 18:02:57 +0700

    Homework4(001)

--- iosSchool_HxH/Modules/Auth/AuthViewController.swift
+++ iosSchool_HxH/Modules/Auth/AuthViewController.swift
@@ -23,7 +23,63 @@ class AuthViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let characterGenerator = CharacterGenerator()
+        var allCharacters: [Character] = []
 
-        view.backgroundColor = .separator
+        for _ in 1...10 {
+            let generatedCharacter = characterGenerator.generateSomeCharacter()
+            allCharacters.append(generatedCharacter)
+        }
+
+        let maleCharacters = allCharacters.filter { $0.gender == .male }
+        let charactersWithA = allCharacters.filter { $0.name.lowercased().contains("a") }
+        let names = ["Ann", "Sergo", "Max", "Vlad", "Kirill"]
+        let charactersFromNames = names.map { name in
+            return Character(
+                id: Int.random(in: 1...99),
+                name: name,
+                species: "Human",
+                image: "\(name.lowercased()).jpeg",
+                url: "CharacterURL: https://www.\(name.lowercased()).com",
+                episode: ["Episode number: 1"],
+                gender: Character.Gender.allCases.randomElement() ?? .unknown,
+                status: Character.Status.allCases.randomElement() ?? .unknown
+            )
+        }
+        let episodeNames = ["Episode 1", "Episode 2", "Episode 3"]
+
+        let charactersForEpisodesMap = episodeNames.map { episode in
+            (0..<1).map { _ in
+                characterGenerator.generateSomeCharacter()
+            }
+        }
+
+        let charactersForEpisodesCompactMap = episodeNames.compactMap { episode in
+            (0..<1).compactMap { _ in
+                characterGenerator.generateSomeCharacter()
+            }
+        }
+        let totalNameLength = allCharacters.reduce(0) { result, character in
+            return result + character.name.count
+        }
+
+        print("Все персонажи:")
+        allCharacters.forEach { print($0.name, "-", $0.gender.rawValue) }
+        print("\nМужские персонажи:")
+        maleCharacters.forEach { print($0.name, "-", $0.gender.rawValue) }
+        print("\nПерсонажи с буквой |а| в имени:")
+        charactersWithA.forEach {print($0.name)}
+        print("\nПерсонажи из массива имен:")
+        charactersFromNames.forEach {print($0.name)}
+        print("\nПерсонажи для каждой серии (map):")
+        charactersForEpisodesMap.forEach {
+            $0.forEach {print($0.name)}
+        }
+        print("\nПерсонажи для каждой серии (compactMap):")
+        charactersForEpisodesCompactMap.forEach {
+            $0.forEach {print($0.name)}
+        }
+        print("Сумма длин всех имен: \(totalNameLength)")
+
     }
 }
--- iosSchool_HxH/ViewController.swift
+++ iosSchool_HxH/ViewController.swift
@@ -26,35 +26,17 @@ class ViewController: UIViewController {
         let forFour = functiongNumberFour()
         print(forFour)
 
-        view.backgroundColor = .darkGray
-        let arrayInt = [1, 2, 3, 4, 5, 6, 7, 8]
-        let result = arrayInt.filter { elem in
-            elem % 2 == 0
-        }
-        print(result)
-        let result1 = arrayInt.filter { $0 % 2 == 0}
-        print(result1)
-        let array: [Double] = [1, 2, 3, 4, 5, 6]
-        _ = array.map { ColorGenerator(alpha: $0) }
-        _ = array.map { alpha -> ColorGenerator? in
-            guard alpha < 1 else {
-                return nil
-            }
-            return ColorGenerator(alpha: alpha)
+        var characters: [Character] = []
+
+        for _ in 1...10 {
+            let generatedCharacter = characterGenerator.generateSomeCharacter()
+            characters.append(generatedCharacter)
         }
 
-        let test1: [ColorGenerator?] = array.map {
-            guard $0 < 1 else {
-                return nil
-            }
-            return ColorGenerator(alpha: $0)
-        }
-        _ = test1.compactMap { $0 }
-        _ = test1.compactMap { $0?.alpha }
-        let arr = [1, 2, 3, 4, 5, 6, 7, 8]
-        let resultReducde = arr.reduce(1) { result, number in
-            result * number
-        }
-        print(resultReducde)
+        let maleCharacters = characters.filter { $0.gender == .male }
+        print("Все персонажи:")
+        characters.forEach { print($0.name, "-", $0.gender.rawValue) }
+        print("\nМужские персонажи:")
+        maleCharacters.forEach { print($0.name, "-", $0.gender.rawValue) }
     }
 }
