14687c67e5012cba9513e341a390d5fa9a6def3b
SergeyChuyko <frodo.logon.com@mail.ru>
2023-11-11 21:51:16 +0700

    Homework5(001)

---
+++ iosSchool_HxH/Modules/Registration/RegistrationAssembly.swift
@@ -0,0 +1,29 @@
+//
+//  RegistrationAssembly.swift
+//  iosSchool_HxH
+//
+//  Created by Sergo on 11.11.2023.
+//
+
+import Foundation
+
+protocol RegistrationAssembly {
+    func registrationVC() -> RegistrationViewController
+    func registrationDataProvider() -> RegistrationDataProvider
+    func registrationCoordinator() -> RegistrationCoordinator
+}
+
+extension Assembly: RegistrationAssembly {
+
+    func registrationVC() -> RegistrationViewController {
+        .init(dataProvider: registrationDataProvider())
+    }
+
+    func registrationDataProvider() -> RegistrationDataProvider {
+        return RegistrationDataProviderImp()
+    }
+
+    func registrationCoordinator() -> RegistrationCoordinator {
+        RegistrationCoordinator(assembly: self, context: .init())
+    }
+}
---
+++ iosSchool_HxH/Modules/Registration/RegistrationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RegistrationViewController.swift
+//  iosSchool_HxH
+//
+//  Created by Sergo on 11.11.2023.
+//
+
+import Foundation
+import UIKit
+
+class RegistrationViewController: UIViewController {
+
+    private let dataProvider: RegistrationDataProvider
+
+    init(dataProvider: RegistrationDataProvider) {
+        self.dataProvider = dataProvider
+
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .orange
+    }
+}
