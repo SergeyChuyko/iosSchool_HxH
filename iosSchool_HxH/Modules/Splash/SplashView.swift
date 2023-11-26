d50a5814e9f70f9acb4576d4f2348f12ebdb4a9f
SergeyChuyko <frodo.logon.com@mail.ru>
2023-11-26 12:37:26 +0700

    Homework10(002)

--- iosSchool_HxH/Modules/Splash/SplashView.swift
+++ iosSchool_HxH/Modules/Splash/SplashView.swift
@@ -14,33 +14,12 @@ protocol SplashView: UIView {
 
 class SplashViewImp: UIView, SplashView {
 
-    @IBOutlet var labelView: UILabel!
-    @IBOutlet var upLabel: UILabel!
-    @IBOutlet var downLabel: UILabel!
-
-    @IBOutlet var substructImageView: UIImageView!
-    @IBOutlet var rectangle17: UIView!
-    @IBOutlet var rectangle18: UIView!
+    @IBOutlet var launchImageView: UIImageView!
 
     func setViewSplash() {
         backgroundColor = UIColor(named: "iceberg-color")
-        substructImageView.image = UIImage(named: "substract")
-
-        labelView.textColor = .white
-        labelView.font = UIFont(name: "PressStart2P-Regular", size: 16)
-        labelView.text = "IT SCHOOL"
-
-        upLabel.textColor = .white
-        downLabel.textColor = .white
-        setViews(to: rectangle17)
-        setViews(to: rectangle18)
-        rectangle18.layer.cornerRadius = 2
+        launchImageView.image = UIImage(named: "launch-image")
     }
 
-    func setViews(to view: UIView) {
-        view.layer.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1).cgColor
-        view.layer.borderWidth = 1
-        view.layer.borderColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1).cgColor
-    }
 
 }
