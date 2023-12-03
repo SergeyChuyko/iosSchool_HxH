5b98a42f58fb2d6241f86512f68c20a4a39a7b70
SergeyChuyko <frodo.logon.com@mail.ru>
2023-12-03 13:15:16 +0700

    Homework11(004)

--- iosSchool_HxH/Modules/Locations/Subviews/LocationsCellData.swift
+++ iosSchool_HxH/Modules/Locations/Subviews/LocationsCellData.swift
@@ -11,13 +11,12 @@ struct LocationsCellData {
     let name: String
     let type: String
     let residents: [String]
-    var populationText: String {
-        return "Население: " + String(residents.count)
-    }
+    let populationText: String
 
     init(location: Location) {
         name = location.name
         type = location.type
         residents = location.residents
+        populationText = "Население: " + String(residents.count)
     }
 }
